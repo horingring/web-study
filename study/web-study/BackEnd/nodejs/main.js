24a4e377d39d15ac209f7560ebe10719c469f0a9
Horingring <dldldksl@naver.com>
2021-03-09 23:04:45 +0900

    0309_nodeJs-by-dailyCoding_5_App_글 목록 출력하기
    - data 디렉토리에 있는 파일들의 이름을 이용해서 글 목록을 생성하는 기능.
      -> 코드와 데이터의 분리. 이렇게 하면 데이터가 추가되면 동적으로 추가된 데이터에 맞추어 글 목록(list)이 생성된다.

--- study/web-study/BackEnd/nodejs/main.js
+++ study/web-study/BackEnd/nodejs/main.js
@@ -11,31 +11,38 @@ var app = http.createServer(function(request, response) {
   console.log(url.parse(_url, true));
 
   if (pathname === '/') {
-    fs.readFile(`data/${title}`, 'utf8', function(err, description) {
-      var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${queryData.id===undefined?'Welcome':title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${queryData.id===undefined?'Welcome':title}</h2>
-          <p>
-          ${queryData.id===undefined?'Hello, Node.js':description}
-          </p>
-        </body>
-        </html>
-        `;
-      response.writeHead(200);
-      response.end(template);
+    fs.readdir('./data', function(err, filelist) {
+      var list = `<ul>`;
+      for (var i = 0; i < filelist.length; i++) {
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      }
+      list = list + `</ul>`;
+
+      fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${queryData.id===undefined?'Welcome':title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${queryData.id===undefined?'Welcome':title}</h2>
+            <p>
+            ${queryData.id===undefined?'Hello, Node.js':description}
+            </p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
     });
+
+
+
   } else {
     response.writeHead(404);
     response.end('Not found');
