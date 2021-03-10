01792dd9e69a4ff05e32b153ec187079b0235cd5
Horingring <dldldksl@naver.com>
2021-03-10 16:36:47 +0900

    0310_nodeJs-by-dailyCoding_6_App_함수를 이용한 정리정돈
    - 반복되는 구문은 함수로 정리정돈 할 수 있다.
    ( * 나의 경우 반복되는 구문은 없지만 정리정돈 했다 )

--- study/web-study/BackEnd/nodejs/main.js
+++ study/web-study/BackEnd/nodejs/main.js
@@ -2,40 +2,52 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+  return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title===undefined?'Welcome':title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist) {
+  var list = `<ul>`;
+  for (var i = 0; i < filelist.length; i++) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list = list + `</ul>`;
+  return list;
+}
+
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
 
   console.log(url.parse(_url, true));
 
   if (pathname === '/') {
     fs.readdir('./data', function(err, filelist) {
-      var list = `<ul>`;
-      for (var i = 0; i < filelist.length; i++) {
-        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      }
-      list = list + `</ul>`;
-
-      fs.readFile(`data/${title}`, 'utf8', function(err, description) {
-        var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${queryData.id===undefined?'Welcome':title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list,
+          `
             <h2>${queryData.id===undefined?'Welcome':title}</h2>
             <p>
             ${queryData.id===undefined?'Hello, Node.js':description}
             </p>
-          </body>
-          </html>
-          `;
+          `
+        );
         response.writeHead(200);
         response.end(template);
       });
