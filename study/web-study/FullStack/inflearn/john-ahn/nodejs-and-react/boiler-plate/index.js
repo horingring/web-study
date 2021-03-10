26980de8c4d0f2f78cae8e533cf97fdb32762cc1
Horingring <dldldksl@naver.com>
2021-03-11 03:01:55 +0900

    0310_Node&React-by-JohnAhn_2_BodyParser&PostMan설치, 회원가입기능, Nodemon 설치
    - BodyParser 모듈 설치
    : 서버에서, 클라이언트로부터 받는 데이터를 쉽게 parse하기 위한 모듈
     
    - PostMan
    : 데스크탑 프로그램. 클라이언트가 없을 때 BackEnd로 원하는 타입의 데이터를 보내줄 수 있다. (클라이언트의 역할)
     
    - 회원가입 기능
    : route로 '/register'라고 (엔드포인트를) 설정하고, 클라이언트가 json 데이터를 보내면, 해당 데이터는 서버에서 parse되어, 이것이 Model(*User)의 생성자의 인자로 들어온다.
    그렇게 생긴 인스턴스의 메소드인 save를 사용하면, 실제 데이터베이스(의 User라는 Model에)에 아까 인자로 받은 정보가 저장된다.
     
    - Nodeman
    : PM2와 같이, 서버의 내용이 변경되면 이를 감지한다. 즉 서버를 껐다가 다시 켤 필요 없이 클라이언트에서 새로고침 하면 변경사항이 바로 적용된다.

--- study/web-study/FullStack/inflearn/john-ahn/nodejs-and-react/boiler-plate/index.js
+++ study/web-study/FullStack/inflearn/john-ahn/nodejs-and-react/boiler-plate/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있도록 함
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 타입으로 된 데이터를 분석해서 가져올 수 있도록 하기 위함
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -17,5 +25,18 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World! 하이하이 하이하이!!!!"));
+
+app.post("/register", (req, res) => {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 db에 넣어준다.
+
+  //bodyParser가 있는 덕분에 req.body로 간단히 요청(req)의 body를 불러올 수 있는 것임
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
