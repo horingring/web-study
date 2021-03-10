d5b6dba7a878a140221a422c0b3ecef18f5c25fa
Horingring <dldldksl@naver.com>
2021-03-10 21:23:21 +0900

    0310_nodeJs-by-dailyCoding_11_App_글수정-수정된 내용 저장
    - 글 수정(완) - 수정된 내용 저장
    : fs.rename(oldPath, newPath, callback(err)) 을 통해 수정된 내용 저장.
     
    * rename이 되지 않고 파일 추가만 되는 문제 발생
     -> rename이 실행되지 않았다는 원인을 찾음
       이유 1) <input type="hidden />에 name을 설정해 주지 않았음..
        ( * 이렇게 되면 서버에서 받은 post 데이터에 당연히 id 정보가 없게 됨)
       이유 2) rename에 인자를 잘못 줌
        -> old/newPath에 `/data/${ ... }` 이렇게 줌
        -> 이게 아니라 `data/${ ... }` 또는 `./data/${ ... }` 이렇게 줘야 함. 문제 해결

--- study/web-study/BackEnd/nodejs/main.js
+++ study/web-study/BackEnd/nodejs/main.js
@@ -120,7 +120,7 @@ var app = http.createServer(function(request, response) {
               action="/update_process"
               method="post"
             >
-              <input type="hidden" value=${title} />
+              <input type="hidden" name="id" value=${title} />
               <p>
                 <input type="text" name="title" placeholder="title" value=${title} />
               </p>
@@ -138,6 +138,26 @@ var app = http.createServer(function(request, response) {
         );
         response.writeHead(200);
         response.end(template);
+      });
+    });
+  } else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body = body + data;
+    });
+    request.on('end', function(err) {
+      var post = qs.parse(body);
+      console.log(post);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`./data/${id}`, `./data/${title}`, function(err) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+          response.writeHead(302, {
+            Location: `/?id=${title}`
+          });
+          response.end('success');
+        });
       });
     });
   } else {
