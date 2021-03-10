c88cb6b518a12081396864b7511dd61b9802c739
Horingring <dldldksl@naver.com>
2021-03-10 17:06:19 +0900

    0310_nodeJs-by-dailyCoding_7_App_글 생성(create) UI 만들기
    - 서버측에서, 클라이언트로부터 받은 요청정보가 들어있는 객체인 url( = require('url') ) 내의 pathname을 통해, 글 생성(Create) 전용 루트인 /create에 맞는 조건문(pathname === '/create'){ ... }을 추가함.
    이 조건문 내에는 앱의 body 부분만 바꿔서 탬플릿을 만듬.

--- study/web-study/BackEnd/nodejs/main.js
+++ study/web-study/BackEnd/nodejs/main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/create">create</a>
       ${body}
     </body>
     </html>
@@ -53,9 +54,36 @@ var app = http.createServer(function(request, response) {
       });
     });
 
-
-
-  } else {
+  }
+  else if(pathname === '/create'){
+    fs.readdir('./data', function(err, filelist) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        var title = 'WEB - Create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list,
+          `
+            <form
+              action="http://localhost:3000/process_create"
+              method="post"
+            >
+              <p>
+                <input type="text" name="title" placeholder="title" />
+              </p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                <input type="submit" />
+              </p>
+            </form>
+          `
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  }
+   else {
     response.writeHead(404);
     response.end('Not found');
   }
