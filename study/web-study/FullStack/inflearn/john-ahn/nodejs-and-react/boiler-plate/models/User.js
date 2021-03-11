2f180d2f68231939edeec7b8ca07b2b724abb7bb
Horingring <dldldksl@naver.com>
2021-03-11 19:58:19 +0900

    0311_Node&React-by-JohnAhn_4_Bcrypt로 비밀번호 암호화
    - bcrypt 모듈 설치.
    : npm install bcrypt --save
     
    - bcrypt로 비밀번호 암호화
    : db에 save 되기 이전에, db에 들어갈 model 인스턴스에 비밀번호 정보가 암호화 되어야 한다.
    즉, 
    1) 클라이언트로부터 받은 정보를 기반으로 User(model) 인스턴스 생성 -> 
    2) 그 인스턴스 내의 '비밀번호' 부분만 암호화  ->
    3) db에 User(model) 정보 저장
    이러한 순서로 진행되는 것이다.
     
    : 위의 2)가 되기 위해서는, User(Model)의 생성파일에 로직을 작성해 주어야 한다. (*즉 User.js에 작성)
    (1) User.js에 bcrypt를 불러온다.
    (2) userSchema.pre('save', 콜백) 으로 'save' 이전에 일어날 함수를 정의해준다. (바로 여기서 비밀번호의 암호화가 진행된다)
    (3) (2) 안의 callback 로직에서 암호화를 진행한다
    (* bcrypt의 경우, 암호화를 위해 salt를 생성해야 함. 그래서 원래 비밀번호(plainPassword)와 생성된 salt를 인자로 hash(암호화)가 이루어짐)
    
    * 나에게 있었던 문제
    postman으로 post 요청을 했으나 오류.
    이유는 bcrypt.genSalt를 getSalt라고 잘못 썼었다..

--- study/web-study/FullStack/inflearn/john-ahn/nodejs-and-react/boiler-plate/models/User.js
+++ study/web-study/FullStack/inflearn/john-ahn/nodejs-and-react/boiler-plate/models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+//bcrypt를 통해 hash를 만들기 위해서는, salt가 필요.
+const bcrypt = require("bcrypt");
+const saltRounds = 10; //salt를 만들기 위해 필요한 값.
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -29,6 +33,30 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+//index.js에서 db로 save를 하기 전에 실행되도록 하는 메소드(on)
+//아래에서 callback에 들어오는 next는 save함수 로직이다.
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  /*
+    user가 password가 아닌 다른 정보(email 등)만을 변경했을 때
+    또 다시 비밀번호 hash가 일어나면 안되므로 조건을 걸어준다.
+  */
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
