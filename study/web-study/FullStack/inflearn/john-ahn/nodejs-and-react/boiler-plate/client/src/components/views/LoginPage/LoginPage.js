615d2737bd4d3f9bdcc0aaf915754c770e5f6f36
Horingring <dldldksl@naver.com>
2021-03-17 01:33:20 +0900

    0316_Node&React-by-JohnAhn_15_React_7_로그인 페이지(Redux)
    - client 로그인 페이지 구현
    - axios를 통한 server 연결
    - Redux를 통한 state 관리
    ( * 아직 잘 모르겠으므로 자세한 설명은 패스 )

--- study/web-study/FullStack/inflearn/john-ahn/nodejs-and-react/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ study/web-study/FullStack/inflearn/john-ahn/nodejs-and-react/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch();
+
+  //--state--
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeEmail = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    let body = {
+      email: email,
+      password: password,
+    };
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push("/");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
   return (
-    <div>
-      <p>LoginPage</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={email} onChange={onChangeEmail}></input>
+        <label>password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+        ></input>
+        <br></br>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
