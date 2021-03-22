4f5284e19d316b30501c55cf82e5ab54f444602d
Horingring <dldldksl@naver.com>
2021-03-23 01:29:43 +0900

    0322_Vue.js-with-zeroCho_3-3_v-for
    - 태그의 v-for 속성
    : 해당 태그를 반복해준다.

--- study/web-study/FrontEnd/javascript/vue/zeroCho/vue-with-zeroCho/3.숫자야구/NumberBaseball.vue
+++ study/web-study/FrontEnd/javascript/vue/zeroCho/vue-with-zeroCho/3.숫자야구/NumberBaseball.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <h1>{{ result }}</h1>
-    <form v-on:submit="onSubmitForm">
-      <input type="text" ref="inputRef" maxlength="4" v-model="input" />
+    <form @submit.prevent="onSubmitForm">
+      <input type="text" ref="inputRef" minlength="4" maxlength="4" v-model="input" />
+      <button type="submit">입력</button>
     </form>
-    <div>시도: {{}}</div>
+    <div>시도: {{tries.length}}</div>
+    <ul>
+      <li v-for="t in tries">
+        <div>{{t.try}}</div>
+        <div>{{t.result}}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,13 +19,21 @@
 export default {
   data() {
     return {
+      tries: [],
       input: "",
       result: "",
     };
   },
   methods: {
     onSubmitForm(e) {
-      e.preventDefault();
+      // e.preventDefault();  
+      //  ㄴ> form의 v-on:submit 속성에 .prevent를 추가함으로써 역할을 대신함
+      this.tries.push({
+        try: this.input,
+        result: '홈런',
+      });
+      this.input = '';
+      this.$refs.inputRef.focus();
     },
   },
 };
