85e97ba87786a12b447b6b447981ce49056615d0
Horingring <dldldksl@naver.com>
2021-03-23 00:44:16 +0900

    0322_Vue.js-with-zeroCho_3-2_Webpack_loader
    - 웹팩 로더 사용하기
    : webpack은 기본적으로 js파일을 번들링함.
    : 그런데 .vue와 같은 파일을 만나면 오류를 일으킴
    : 그래서 이런 vue와 같은 파일까지 번들링이 되도록 변환해 주는 역할을 loader가 해줌
       -> vue-loader 설치
     
    - 바로 rules(in webpack.config.js)에서 위의 vue-loader 사용을 설정할 수 있다.
      -> { test: .vue의 정규표현식, loader: "vue-loader" }
     
    - vue-template-compiler
    : 또 vue-template-compiler가 요구됨.
    : *** 이때 vue와 vue-template-compiler는 버전이 같아야만 한다고 함 ( 즉, 추후 vue의 버전을 업그레이드 한다면, vue-template-compiler도 같은 버전으로 업그레이드 해 주어야 함 ) ***
    
    * 기타
    resolve: { extensions: [ '.js', '.vue'] }
      -> 위 확장자를 입력함으로써, js 파일 등에서 다른 파일을 import 시, ...from '파일명.vue' 에서 .vue를 입력하지 않아도 됨(설정하면 확장자를 입력하지 않아도 됨)

---
+++ study/web-study/FrontEnd/javascript/vue/zeroCho/vue-with-zeroCho/3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+    <form v-on:submit="onSubmitForm">
+      <input type="text" ref="inputRef" maxlength="4" v-model="input" />
+    </form>
+    <div>시도: {{}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      result: "",
+    };
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
+
+<style></style>
--- study/web-study/FrontEnd/javascript/vue/zeroCho/vue-with-zeroCho/3.숫자야구/webpack.config.js
+++ study/web-study/FrontEnd/javascript/vue/zeroCho/vue-with-zeroCho/3.숫자야구/webpack.config.js
@@ -1,13 +1,29 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require("path");
+
 module.exports = {
+  mode: 'development',  //개발용. (*배포는 production)
+  devtool: 'eval',  //개발용. (*배포는 hidden-source-map)
+  resolve: {
+    extensions: ['.js', '.vue'], //확장자를 처리해줌
+  },
   entry: {
-    app: "./main.js",
+    app: path.join(__dirname, "main.js"),
   },
   module: {
-    rules: [{}],
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
   },
-  plugins: [],
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
   output: {
     filename: "[name].js",
-    path: "./dist",
+    //path.join(__dirname)은 현재 디렉토리의 절대경로를 return한다.
+    path: path.join(__dirname, "dist"), 
   },
 };
