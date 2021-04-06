25a403ef1a6d6decb1fb464d08c8ffd3aaec0e3e
Horingring <dldldksl@naver.com>
2021-04-06 18:22:17 +0900

    0406_interactiveWeb-with-1분코딩_10-2_section-3_눈속임 영역 만들기
    - scroll down시 일분이 이미지의 width가 점점 넓어지는 효과
    : 사실은 눈속임 영역을 일분이 이미지 양쪽에 각각 만들어, 이를 화면 밖으로 이동시켜 주는 방식.
    : 이 영역을 그려내기 위해서, 일분이 이미지가 그려지는 영역의 실제 길이(width, height)를 알 필요가 있다. 이를 계산하기 위해,
    window.innerWidth/canvasScaleRatio
    이런 식으로 줄인 비율만큼 다시 나누어 주어야 한다.

--- study/web-study/FrontEnd/javascript/interactive-web/1minute-coding/apple-clone/js/main.js
+++ study/web-study/FrontEnd/javascript/interactive-web/1minute-coding/apple-clone/js/main.js
@@ -112,7 +112,10 @@
         ],
         images: [],
       },
-      values: {},
+      values: {
+        rect1X: [0, 0, { start: 0, end: 0 }],
+        rect2X: [0, 0, { start: 0, end: 0 }],
+      },
     },
   ];
 
@@ -420,6 +423,30 @@
 
         objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
         objs.context.drawImage(objs.images[0], 0, 0);
+
+        const recalculatedInnerWidth = window.innerWidth / canvasScaleRatio;
+        const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+        const whiteRectWidth = recalculatedInnerWidth * 0.15;
+        values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+        values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+        values.rect2X[0] =
+          values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+        values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+        //좌우 흰색 박스 그리기
+        objs.context.fillRect(
+          values.rect1X[0],
+          0,
+          whiteRectWidth,
+          recalculatedInnerHeight
+        );
+        objs.context.fillRect(
+          values.rect2X[0],
+          0,
+          whiteRectWidth,
+          recalculatedInnerHeight
+        );
 
         break;
     }
