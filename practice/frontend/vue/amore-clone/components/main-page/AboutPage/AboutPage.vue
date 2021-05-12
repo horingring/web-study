0d76c6d3b730f49b9effbe438c05d6e013e0e572
Horingring <dldldksl@naver.com>
2021-05-13 05:11:57 +0900

    0512_Vue_Prac_Clone_Amore_5_AboutPage.vue 공통 탬플릿 일부분 코딩
    - Commit 제목과 내용 같음

--- practice/frontend/vue/amore-clone/components/main-page/AboutPage/AboutPage.vue
+++ practice/frontend/vue/amore-clone/components/main-page/AboutPage/AboutPage.vue
@@ -1,11 +1,31 @@
 <template>
-  <main>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <h1>About Page</h1>
+  <main class="about-page">
+    <section>
+      <h1>About Us</h1>
+      <p>
+        우리만이 창조할 수 있는 아름다움으로 여성의 삶을 아름답게 하는 것을
+        우리의 소명으로 생각합니다.<br />
+        ‘A MORE Beautiful World’라는 비전 아래 모든 여성들이 저마다 꿈꾸는 삶을
+        누리며 더 아름다운 세상을 만들어 갈 수<br />
+        있도록 여성의 아름다운 삶에 기여합니다.
+      </p>
+    </section>
+    <section>
+      <ul>
+        <li>
+          <div>Vision</div>
+        </li>
+        <li>
+          <div>Commitment</div>
+        </li>
+        <li class="last">
+          <div>공익재단</div>
+        </li>
+      </ul>
+      <div class="about_banner">
+        <span>Vision</span>
+      </div>
+    </section>
   </main>
 </template>
 
